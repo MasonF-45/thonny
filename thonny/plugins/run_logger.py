a335606ca05fc5ea235cbfeccf0540136b54e792
Mason <mason.fortney@student.cune.edu>
2026-02-03 20:03:38 -0600

    .

--- thonny/plugins/run_logger.py
+++ thonny/plugins/run_logger.py
@@ -1,4 +1,13 @@
-import thonny, os
-print ('here')
-print(os.path.dirname(thonny.__file__))
+from thonny import get_workbench
+
+def show_toolbar_children(event=None):
+    wb = get_workbench()
+    tb = wb.get_toolbar()
+    print("\n=== TOOLBAR CHILDREN ===")
+    for name, widget in tb.children.items():
+        print(name, "->", widget)
+
+def load_plugin():
+    print("TOOLBAR PROBE LOADED")
+    get_workbench().bind("WorkbenchReady", show_toolbar_children, True)
 
